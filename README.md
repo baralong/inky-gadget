2f135ffedbf051299896487fb0129bd670b7bb11
Doug Paice <doug.paice@baralong.org>
2022-03-31 13:44:23 +0800

    get files for the USB Gadget service

--- README.md
+++ README.md
@@ -1,2 +1,9 @@
 # inky-gadget
 Scripts etc for the pi zero with inkyPhat
+
+This displays:
+1) The name of Raspberry Pi
+2) The IP address from wifi and/or USB ethernet gadget
+3) Any connected sessions: tty, if connected via the USB, or the ip if a different connection
+
+This requires things are setup similarly to the ethernet and mass storage gadgets as described here https://github.com/thagrol/Guides, also that the backing file for the USB gadget is `/usbdisk.img` mounted at `/usbdisk.d`
---
+++ usbGadget.sh
@@ -0,0 +1,78 @@
+#!/bin/bash
+
+##
+## load libcomposite and configure an ethernet gadget
+## run at boot from systemd, cron, or rc.local
+## must be run as root or with sudo
+##
+
+#
+# configuration parameters.
+# sensible defaults but change as desired
+GADGETDIR='pigadget' # full path should not be supplied
+SERIAL=`cat /proc/cpuinfo | grep Serial | cut -d ' ' -f 2` # Pi's serial number
+HOSTPREFIX="02"     # hex, two digits only
+DEVICEPREFIX="06"   # hex, two digits only
+MANUFACTURER="Baralong"
+PRODUCT='USB Gadget Device'
+
+# calculate MAC addresses
+padded='00000000000000'$SERIAL
+for i in -10 -8 -6 -4 -2; do
+    basemac=$basemac':'${padded: $i:2}
+done
+hostmac=$HOSTPREFIX$basemac
+devmac=$DEVICEPREFIX$basemac
+
+# am I root?
+if [ $# != 0 ]; then
+	echo "Must be root" >&2
+	exit 1
+fi
+
+modprobe libcomposite
+if [ $? -ne 0 ]; then
+    echo "unable to load libcomposite, exiting"
+    exit 1
+fi
+
+mkdir -p /sys/kernel/config/usb_gadget/$GADGETDIR
+cd /sys/kernel/config/usb_gadget/$GADGETDIR
+echo 0x1d6b > idVendor # Linux Foundation
+echo 0x0104 > idProduct # Multifunction Composite Gadget
+echo 0x0100 > bcdDevice # v1.0.0
+echo 0x0200 > bcdUSB # USB2
+mkdir -p strings/0x409
+echo $SERIAL > strings/0x409/serialnumber
+echo $MANUFACTURER > strings/0x409/manufacturer
+echo $PRODUCT > strings/0x409/product
+mkdir -p configs/c.1/strings/0x409
+echo "Config 1: ECM network" > configs/c.1/strings/0x409/configuration
+echo 250 > configs/c.1/MaxPower
+
+# Add Functions
+
+# serial
+mkdir -p functions/acm.usb0
+ln -s functions/acm.usb0 configs/c.1/  
+
+# Ethernet
+mkdir -p functions/ecm.usb0
+echo $hostmac > functions/ecm.usb0/host_addr
+echo $devmac > functions/ecm.usb0/dev_addr
+ln -s functions/ecm.usb0 configs/c.1/
+
+# mass storage
+FILE=/usbdisk.img
+mkdir -p ${FILE/img/d}
+mount -o loop -t exfat $FILE ${FILE/img/d}
+mkdir -p functions/mass_storage.usb0
+echo 1 > functions/mass_storage.usb0/stall
+echo 0 > functions/mass_storage.usb0/lun.0/cdrom
+echo 0 > functions/mass_storage.usb0/lun.0/ro
+echo 0 > functions/mass_storage.usb0/lun.0/nofua
+echo $FILE > functions/mass_storage.usb0/lun.0/file
+ln -s functions/mass_storage.usb0 configs/c.1/
+
+# End Functions
+ls /sys/class/udc > UDC
